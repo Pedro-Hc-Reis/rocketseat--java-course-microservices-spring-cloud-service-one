c99708a094b428c0c332de149f97256308dd68e3
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2025-02-12 09:58:15 -0300

    Aula 15

--- src/main/java/br/com/rocketseat/service_tasks/Application.java
+++ src/main/java/br/com/rocketseat/service_tasks/Application.java
@@ -2,12 +2,16 @@ package br.com.rocketseat.service_tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableFeignClients
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run( Application.class, args);
-	}
+    public static void main ( String[] args ) {
+        SpringApplication.run ( Application.class , args );
+    }
 
 }
--- src/main/java/br/com/rocketseat/service_tasks/TaskService.java
+++ src/main/java/br/com/rocketseat/service_tasks/TaskService.java
@@ -1,7 +1,12 @@
 package br.com.rocketseat.service_tasks;
 
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
+
+@Service
 public class TaskService {
 
     private final TasksRepository tasksRepository;
@@ -13,7 +18,8 @@ public class TaskService {
     }
 
     public void sendNotificationForDueTasks ( ) {
-        List<TasksEntity> tasks = tasksRepository.findDueTasks ( );
+        LocalDateTime deadline = LocalDateTime.now ( ).plusDays ( 1 );
+        List<TasksEntity> tasks = tasksRepository.findTasksDueWithinDeadline ( deadline );
         for (TasksEntity task : tasks) {
             NotificationRequest request = new NotificationRequest ( "Sua tarefa: " + task.getTitle ( ) + "está prestes a vencer" , task.getEmail ( ) );
             notificationClient.sendNotification ( request );
